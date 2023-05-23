d6513a2b52318a04af62b39fc78cb41262d7981d
yonipels <pels.yona@mail.huji.ac.il>
2023-05-23 21:36:24 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,25 @@ const cors = require('cors')
 
 // App Configuration
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
-}
-
-// const corsOptions = {
-//     origin: [
-//         'http://127.0.0.1:8080',
-//         'http://localhost:8080',
-//         'http://127.0.0.1:3000',
-//         'http://localhost:3000'
-//     ],
-//     credentials: true
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.resolve(__dirname, 'public')))
+// } else {
+//     const corsOptions = {
+//         origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+//         credentials: true
+//     }
+//     app.use(cors(corsOptions))
 // }
+
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:8080',
+        'http://localhost:8080',
+        'http://127.0.0.1:3000',
+        'http://localhost:3000'
+    ],
+    credentials: true
+}
 
 
 app.use(cors(corsOptions))
@@ -170,11 +170,11 @@ app.get('/**', (req, res) => {
 
 
 // Listen will always be the last line in our server!
-// app.listen(3030, () => console.log('Server listening on port 3030!'))
-const port = process.env.PORT || 3030
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-app.listen(port, () => {
-    console.log(`App listening on port ${port}!`)
-});
+app.listen(3030, () => console.log('Server listening on port 3030!'))
+// const port = process.env.PORT || 3030
+// app.get('/**', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+// app.listen(port, () => {
+//     console.log(`App listening on port ${port}!`)
+// });
